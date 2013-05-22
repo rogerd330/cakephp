31dee7f00e1ba0ef45953a97010e38412d550fc3
Roger Dickey <dickeyr@gmail.com>
2013-05-22 10:36:13 -0500

    Generate schema for Categories table.

---
+++ app/Plugin/ContentManagement/Config/Schema/schema.php
@@ -0,0 +1,20 @@
+<?php 
+class ContentManagementSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+}
