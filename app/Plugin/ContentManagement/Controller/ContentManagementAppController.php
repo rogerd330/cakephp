e572172c633c599a4fd9fbe9162f369b625c7e74
Roger Dickey <dickeyr@gmail.com>
2013-05-28 17:19:26 -0500

    Show Options menu on site admin.

--- app/Plugin/ContentManagement/Controller/ContentManagementAppController.php
+++ app/Plugin/ContentManagement/Controller/ContentManagementAppController.php
@@ -8,6 +8,7 @@ class ContentManagementAppController extends AppController {
             array('anchor' => 'Posts', 'link' => array('controller' => 'Posts', 'action' => 'index')),
             array('anchor' => 'Events', 'link' => array('controller' => 'Events', 'action' => 'index')),
             array('anchor' => 'Categories', 'link' => array('controller' => 'Categories', 'action' => 'index')),
+            array('anchor' => 'Options', 'link' => array('controller' => 'Options', 'action' => 'index')),
         );
         return $nav;
     }
