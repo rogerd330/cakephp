aaaf2b2adab4d6dc911af1f9b34acacf9c395bb6
Roger Dickey <dickeyr@gmail.com>
2014-03-03 21:02:28 -0600

    Simplify the layout of the default CMS admin links.

--- app/Plugin/ContentManagement/Controller/ContentManagementAppController.php
+++ app/Plugin/ContentManagement/Controller/ContentManagementAppController.php
@@ -5,10 +5,17 @@ App::uses('AppController', 'Controller');
 class ContentManagementAppController extends AppController {
     function getAdminNav() {
         $nav = array(
-            array('anchor' => 'Posts', 'link' => array('controller' => 'Posts', 'action' => 'index')),
-            array('anchor' => 'Events', 'link' => array('controller' => 'Events', 'action' => 'index')),
-            array('anchor' => 'Categories', 'link' => array('controller' => 'Categories', 'action' => 'index')),
-            array('anchor' => 'Options', 'link' => array('controller' => 'Options', 'action' => 'index')),
+            'left' => array(
+                array('anchor' => 'Posts', 'link' => array('controller' => 'Posts', 'action' => 'index')),
+                array('anchor' => 'Events', 'link' => array('controller' => 'Events', 'action' => 'index')),
+                array('anchor' => 'Categories', 'link' => array('controller' => 'Categories', 'action' => 'index')),
+            ),
+            'right' => array(
+                array('anchor' => 'Settings', 'dropdown' => true, 'links' => array(
+                   array('anchor' => 'Options', 'link' => array('controller' => 'Options', 'action' => 'index')),
+                )),
+                array('anchor' => 'Log Out', 'link' => array('controller' => 'users', 'action' => 'logout')),
+            )
         );
         return $nav;
     }
