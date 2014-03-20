45e2eea41e4b54dcb89acc96527cd5838ee07f9f
Roger Dickey <dickeyr@gmail.com>
2014-03-19 23:38:18 -0500

    Give form post-back handler logic a place to live.

---
+++ app/Plugin/ContentManagement/Controller/FormsController.php
@@ -0,0 +1,15 @@
+<?php
+ /**
+ * Created by Roger Dickey, Jr
+ * rdickey@whytespyder.com
+ * 3/19/14 11:34 PM
+ */
+
+App::uses('AppController', 'Controller');
+
+class FormsController extends ContentManagementAppController {
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+}
