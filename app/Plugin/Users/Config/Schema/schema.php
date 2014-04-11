9f4b2fc5a50d0a0b8c162b34b42cf311cee7fd56
Roger Dickey <dickeyr@gmail.com>
2014-04-11 13:00:12 -0500

    Create the schema for the Users plugin.

---
+++ app/Plugin/Users/Config/Schema/schema.php
@@ -0,0 +1,25 @@
+<?php 
+class UserSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'enabled' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'group_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'login' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 25, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+}
