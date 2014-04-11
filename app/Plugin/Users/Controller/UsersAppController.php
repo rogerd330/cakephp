a0ff3fbe5f4acc4796a933d9299924c8a02284e4
Roger Dickey <dickeyr@gmail.com>
2014-04-11 11:54:47 -0500

    Enable site wide authentication. Add a logout link to the site admin nav.

--- app/Plugin/Users/Controller/UsersAppController.php
+++ app/Plugin/Users/Controller/UsersAppController.php
@@ -3,5 +3,34 @@
 App::uses('AppController', 'Controller');
 
 class UsersAppController extends AppController {
+    public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginAction' => array('controller' => 'users', 'action' => 'login', 'plugin' => 'users', 'admin' => false),
+            'loginRedirect' => array('controller' => 'pages', 'action' => 'index', 'plugin' => 'content_management', 'admin' => true),
+            'authError' => 'You must login first!',
+            'flash' => array('element' => 'flash_message', 'key' => 'auth', 'params' => array()),
+            'authenticate' => array(
+                'Blowfish' => array(
+                    'fields' => array(
+                        'username' => 'login',
+                    ),
+                    'scope' => array(
+                        'enabled' => true,
+                    )
+                )
+            ),
+        ),
+    );
 
+    function getAdminNav() {
+        $nav = array(
+            'left' => array(
+            ),
+            'right' => array(
+                array('anchor' => 'Log Out', 'link' => array('controller' => 'users', 'action' => 'logout')),
+            )
+        );
+        return $nav;
+    }
 }
