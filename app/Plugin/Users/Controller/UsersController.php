78484e98a285e4f1639c81269a82cf699d309b8e
Roger Dickey <dickeyr@gmail.com>
2014-04-11 11:26:25 -0500

    Bake the Users controller.

---
+++ app/Plugin/Users/Controller/UsersController.php
@@ -0,0 +1,103 @@
+<?php
+App::uses('UsersAppController', 'Users.Controller');
+/**
+ * Users Controller
+ *
+ * @property User $User
+ * @property PaginatorComponent $Paginator
+ */
+class UsersController extends UsersAppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->User->recursive = 0;
+		$this->set('users', $this->paginate());
+	}
+
+/**
+ * admin_view method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function admin_view($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$this->set('user', $this->User->read(null, $id));
+	}
+
+/**
+ * admin_add method
+ *
+ * @return void
+ */
+	public function admin_add() {
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->setFlash(__('The user has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->setFlash(__('The user could not be saved. Please, try again.'), false);
+			}
+		}
+	}
+
+/**
+ * admin_edit method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function admin_edit($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->User->save($this->request->data)) {
+				$this->setFlash(__('The user has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->setFlash(__('The user could not be saved. Please, try again.'), false);
+			}
+		} else {
+			$this->request->data = $this->User->read(null, $id);
+		}
+	}
+
+/**
+ * admin_delete method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function admin_delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->User->delete()) {
+			$this->setFlash(__('User deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->setFlash(__('User was not deleted'), false);
+		$this->redirect(array('action' => 'index'));
+	}
+}
---
+++ app/Plugin/Users/Test/Case/Controller/UsersControllerTest.php
@@ -0,0 +1,107 @@
+<?php
+/* Users Test cases generated on: 2014-04-11 11:25:51 : 1397233551*/
+App::uses('UsersController', 'Users.Controller');
+
+/**
+ * TestUsersController *
+ */
+class TestUsersController extends UsersController {
+/**
+ * Auto render
+ *
+ * @var boolean
+ */
+	public $autoRender = false;
+
+/**
+ * Redirect action
+ *
+ * @param mixed $url
+ * @param mixed $status
+ * @param boolean $exit
+ * @return void
+ */
+	public function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+/**
+ * UsersController Test Case
+ *
+ */
+class UsersControllerTestCase extends CakeTestCase {
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array('plugin.users.user', 'plugin.users.option', 'plugin.users.meta');
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Users = 	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Users);
+
+		parent::tearDown();
+	}
+
+/**
+ * testAdminIndex method
+ *
+ * @return void
+ */
+	public function testAdminIndex() {
+
+	}
+
+/**
+ * testAdminView method
+ *
+ * @return void
+ */
+	public function testAdminView() {
+
+	}
+
+/**
+ * testAdminAdd method
+ *
+ * @return void
+ */
+	public function testAdminAdd() {
+
+	}
+
+/**
+ * testAdminEdit method
+ *
+ * @return void
+ */
+	public function testAdminEdit() {
+
+	}
+
+/**
+ * testAdminDelete method
+ *
+ * @return void
+ */
+	public function testAdminDelete() {
+
+	}
+
+}
